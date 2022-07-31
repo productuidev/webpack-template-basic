3740674a23bd07c9e6551d88adbc640aab334a99
productuidev <productuidev@gmail.com>
2022-07-31 19:35:45 +0900

    vue3 .eslintrc.js

--- vue3-webpack-template/.eslintrc.js
+++ vue3-webpack-template/.eslintrc.js
@@ -26,6 +26,15 @@ module.exports = {
     "vue/html-closing-bracket-newline": ["error", {
       "singleline": "never",
       "multiline": "never"
+    }],
+    "vue/html-self-closing": ["error", {
+      "html": {
+        "void": "always",
+        "normal": "never",
+        "component": "always"
+      },
+      "svg": "always",
+      "math": "always"
     }]
   }
 }
